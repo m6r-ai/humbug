ebf7c94998880425bd69f8ab80700db796ccd2cf
Dave Hudson <dave-hudson@users.noreply.github.com>
2024-12-17 15:19:16 +0000

    Only update if an event takes > 20ms

--- src/humbug/__main__.py
+++ src/humbug/__main__.py
@@ -24,7 +24,7 @@ class HumbugApplication(QApplication):
         ret = QApplication.notify(self, receiver, event)
         end = time.monotonic()
         elapsed_time = (end - start) * 1000
-        if elapsed_time > 2:
+        if elapsed_time > 20:
             rel_start = start - self._start_time
             print(f"{rel_start:.3f}: processing event type {event_type} for object {receiver_name} took {elapsed_time:.3f} msec")
 
