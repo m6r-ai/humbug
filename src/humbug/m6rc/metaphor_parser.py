99f5167cf200936be8fb65becbea5df541fc8df0
Dave Hudson <dave-hudson@users.noreply.github.com>
2025-04-09 20:10:16 +0100

    Remove copyright notices
    * They're in the supporting files anyway.

--- src/humbug/m6rc/metaphor_parser.py
+++ src/humbug/m6rc/metaphor_parser.py
@@ -1,24 +1,10 @@
-# Copyright 2024 M6R Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import glob
 import os
 from pathlib import Path
 from typing import List, Set, cast
 
 from humbug.m6rc.metaphor_token import MetaphorToken, MetaphorTokenType
-from humbug.m6rc.embed_lexer import MetaphorEmbedLexer
+from humbug.m6rc.metaphor_embed_lexer import MetaphorEmbedLexer
 from humbug.m6rc.metaphor_lexer import MetaphorLexer
 from humbug.m6rc.metaphor_ast_node import (
     MetaphorRootNode, MetaphorTextNode, MetaphorCodeNode,
