bf9e17501a328a977680974ad90acc4fcd6f77f9
Dave Hudson <dave-hudson@users.noreply.github.com>
2025-04-09 20:10:05 +0100

    Use the Metaphor namespace

--- src/humbug/m6rc/metaphor_embed_lexer.py
+++ src/humbug/m6rc/metaphor_embed_lexer.py
@@ -1,22 +1,8 @@
-# Copyright 2024 M6R Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from typing import Dict, List
 
 from humbug.m6rc.metaphor_token import MetaphorToken, MetaphorTokenType
 
-class EmbedLexer:
+class MetaphorEmbedLexer:
     """
     Lexer for handling embedded content like code blocks.
     """
@@ -68,7 +54,7 @@ class EmbedLexer:
 
     def __init__(self, input_text: str, filename: str) -> None:
         """
-        Initialize the EmbedLexer for handling embedded content.
+        Initialize the MetaphorEmbedLexer for handling embedded content.
 
         Args:
             input_text (str): The text content to be lexically analyzed
